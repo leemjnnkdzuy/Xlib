4eaa67a5ea04dc701b39fbba7c4884198250e62f
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-03-01 01:15:47 +0700

    m

---
+++ tools/HexToText/js/HexToText.js
@@ -0,0 +1,14 @@
+function convertHexToText() {
+    const hexInput = document.getElementById('hexInput').value;
+    const textOutput = document.getElementById('textOutput');
+
+    try {
+        const hexString = hexInput.replace(/\s/g, '');
+        const hexArray = hexString.match(/.{1,2}/g);
+        const textString = hexArray.map(hex => String.fromCharCode(parseInt(hex, 16))).join('');
+
+        textOutput.value = textString;
+    } catch (error) {
+        textOutput.value = 'Invalid hex input.';
+    }
+}
