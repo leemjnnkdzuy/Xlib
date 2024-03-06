4ea68b967c5a5218ff0b7d14e67826643c21ba56
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-03-07 00:31:47 +0700

    setup admin login

--- accounts/php/login.php
+++ accounts/php/login.php
@@ -1,4 +1,29 @@
 <?php
-    echo $_POST['email'];
-    echo $_POST['password'];
+    session_start();
+
+    if (isset($_POST['login'])) {
+        if(isset($_POST['email']) && isset($_POST['password'])) {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $remember = isset($_POST['remember']) ? true : false;
+
+
+            $hashedPassword = password_hash("admin", PASSWORD_DEFAULT); 
+            if ($email == "admin@admin.com" && password_verify($password, $hashedPassword)) {
+                $_SESSION['email'] = $email;
+
+
+                if ($remember) {
+                    
+                }
+
+                header('location:admin.php');
+                exit();
+            } else {
+                echo "Sai tài khoản hoặc mật khẩu";
+                header('location:../login.html');
+                exit();
+            }
+        }
+    }
 ?>
